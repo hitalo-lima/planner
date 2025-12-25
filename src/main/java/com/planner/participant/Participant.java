a89455768b44783bb6b9196cb429ab53f144b543
hitalo-lima <hitalolima173@gmail.com>
2025-12-25 11:38:42 -0300

    feat: add Participant entity, repository, and database migration for trip participants

---
+++ src/main/java/com/planner/participant/Participant.java
@@ -0,0 +1,46 @@
+package com.planner.participant;
+
+import java.util.UUID;
+
+import com.planner.trip.Trip;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "participants")
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
