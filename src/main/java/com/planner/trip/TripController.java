c25fdeeaa9f973ccd32a25494b40f13ddacb0af7
hitalo-lima <hitalolima173@gmail.com>
2025-12-25 10:03:54 -0300

    feat: add updateTrip endpoint to modify trip details

--- src/main/java/com/planner/trip/TripController.java
+++ src/main/java/com/planner/trip/TripController.java
@@ -1,5 +1,7 @@
 package com.planner.trip;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,6 +15,7 @@ import com.planner.participant.ParticipantService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/trips")
@@ -43,4 +46,23 @@ public class TripController {
 
 		return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload) {
+		Optional<Trip> trip = this.tripRepository.findById(id);
+
+		if (trip.isPresent()) {
+			Trip rawTrip = trip.get();
+			rawTrip.setStartsAt(LocalDateTime.parse(payload.startsAt(), DateTimeFormatter.ISO_DATE_TIME));
+			rawTrip.setEndsAt(LocalDateTime.parse(payload.endsAt(), DateTimeFormatter.ISO_DATE_TIME));
+			rawTrip.setDestination(payload.destination());
+
+			this.tripRepository.save(rawTrip);
+
+			return ResponseEntity.ok(rawTrip);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 }
