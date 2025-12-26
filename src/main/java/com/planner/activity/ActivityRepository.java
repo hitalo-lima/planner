acb3643c407a421ad8ad582f7c3e95c9e56b3164
hitalo-lima <hitalolima173@gmail.com>
2025-12-26 12:28:09 -0300

    feat: add Activity entity and repository, and create activities table in the database

---
+++ src/main/java/com/planner/activity/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.planner.activity;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+
+}
